75bac95a18ce19e922127bc330efee65b9082337
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-05 17:07:26 +0600

    init

---
+++ src/types/index.d.ts
@@ -0,0 +1,13 @@
+export type SiteConfig = {
+  title: string;
+  name: string;
+  description: string;
+  keywords: string[];
+  author: string;
+  siteUrl: string;
+  ogImage: string;
+  links: {
+    github: string;
+    twitter: string;
+  };
+}
