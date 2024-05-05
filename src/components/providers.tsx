b17aaa59bacbfef980853b8386420175bd9a67bc
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-05 22:03:04 +0600

    chore: Update providers to include Solana wallet integration and theme provider

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,9 +1,28 @@
-import { ThemeProvider } from "@/components/theme/theme-provider"
+import { ThemeProvider } from "@/components/theme/theme-provider";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { Toaster } from "@/components/ui/sonner";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import * as web3 from "@solana/web3.js";
+import { useMemo } from "react";
+import "@solana/wallet-adapter-react-ui/styles.css";
 
 export default function Proivders({ children }: { children: React.ReactNode }) {
+  const endpoint = web3.clusterApiUrl("devnet");
+  const wallets = useMemo(() => [], []);
+
   return (
-    <ThemeProvider>
-      {children}
-    </ThemeProvider>
-  )
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets}>
+        <WalletModalProvider>
+          <ThemeProvider>
+            <Toaster position="top-center" richColors toastOptions={{}} />
+            {children}
+          </ThemeProvider>
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
 }
