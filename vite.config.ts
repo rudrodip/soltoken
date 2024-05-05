b253386045ccda73c7fbecd317d8f8bed07094fe
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-05 22:02:16 +0600

    chore: Add nodePolyfills plugin to Vite config for compatibility with older browsers

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), nodePolyfills()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
