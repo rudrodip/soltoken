c68e21b4ef5c3e9ab3dcd852f9280c39b572e413
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-05 17:07:07 +0600

    site config

---
+++ src/config/site.config.ts
@@ -0,0 +1,19 @@
+import type { SiteConfig } from '@/types'
+
+export const siteConfig: SiteConfig = {
+  title: "SolToken",
+  name: "SolToken",
+  description: "Example application of interacting with Solana SPL Token using React",
+  keywords: [
+    "Solana",
+    "SPL Token",
+    "React",
+  ],
+  author: "rds_agi",
+  siteUrl: "https://soltoken.vercel.app",
+  ogImage: "https://soltoken.vercel.app/og.png",
+  links: {
+    github: "https://github.com/rudrodip/soltoken",
+    twitter: "https://twitter.com/rds_agi",
+  }
+}
